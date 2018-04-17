8553e3e25202ee152bd7bbd660e1132cff7b55d1
Silas Berger <berger.silu@hotmail.com>
2018-04-17 09:07:17 +0200

    added some comments

--- markov-model-builder.py
+++ markov-model-builder.py
@@ -8,20 +8,23 @@ INDEX_OFFSET = ord('a')
 
 
 def train(file):
+	# init counts with 0
 	initial_state_vector = np.zeros(26, dtype=np.float32)
 	follow_matrix = np.zeros([26, 26], dtype=np.float32)
 
+	# iterate through lines in training set
 	for line in tqdm(file):
 		line = line.lower().strip()
-		initial_state_vector[char_to_index(line[0])] += 1
-		for c, c_follow in zip(line[0:-1], line[1:]):
-			follow_matrix[char_to_index(c), char_to_index(c_follow)] += 1
+		initial_state_vector[char_to_index(line[0])] += 1  # increment count for starting char in initial chars
+		for c, c_follow in zip(line[0:-1], line[1:]):  # iterate through all chars of the line and their respective follow char
+			follow_matrix[char_to_index(c), char_to_index(c_follow)] += 1  # increment count for "after c follows c_follow"
 
-	initial_state_likelihood_vector = normalize(initial_state_vector)
-	follow_likelihood_matrix = normalize(follow_matrix)
+	initial_state_likelihood_vector = normalize(initial_state_vector)  # convert counts to likelihoods
+	follow_likelihood_matrix = normalize(follow_matrix)  # convert counts to likelohoods (row-wise)
 	return initial_state_likelihood_vector, follow_likelihood_matrix
 
 
+# convert char value to array index
 def char_to_index(c):
 	return ord(c) - INDEX_OFFSET
 
@@ -30,12 +33,13 @@ def normalize(x):
 	# normalize initial state vector
 	if(len(x.shape) == 1):
 		return x / x.sum()
-	# otherwise, normalize follow matrix
+	# otherwise, normalize follow matrix, row-wise
 	for i in range(x.shape[0]):
 		x[i] = x[i] / x[i].sum()
 	return x 
 
 
+# if args count doesn't match expected count, print usage and terminate
 def verify_args_or_die(argv):
 	if len(argv) != 2:
 		print("Usage: markov-model-builder.py <input_filename> <output_filename_no_extension>")
@@ -53,6 +57,7 @@ def main(argv):
 		initial_states, follow = train(infile)
 	
 	print("saving model")
+	# export np arrays
 	np.save(output_filename + "_initial", initial_states)
 	np.save(output_filename + "_follow", follow)
 	
--- random-name-generator.py
+++ random-name-generator.py
@@ -9,40 +9,53 @@ INDEX_OFFSET = ord('a')
         
 
 def generate(length, model_base_name):
+    # convert chars string into list of separate chars
     choices = []
     for c in chars:
         choices.append(c)
 
+    # load numpy model parts
     initial = np.load(model_base_name + "_initial.npy")
     follow = np.load(model_base_name + "_follow.npy")
+
+    # find a random initial char
     current_char = initial_char(choices, initial)
+
+    # start random name with initial char
     name = current_char.upper()
+
+    # append length-1 follow-chars
     for i in range(length-1):
         follow_c = next_char(current_char, choices, follow)
         name += follow_c
         current_char = follow_c
+        
     return name
 
 
+# get next random char based on likelihoods for current char (given by loaded model)
 def next_char(current, choices, model):
     likelihood_vector = model[char_to_index(current)]
     return np.random.choice(choices, p=likelihood_vector)
 
 
+# get random starting char based on initial char likelihoods (given by loaded model)
 def initial_char(choices, model):
     return np.random.choice(choices, p=model)
 
 
+# convert char value to array index
 def char_to_index(c):
     return ord(c) - INDEX_OFFSET
 
 
 def main(argv):
     try:
+        # check and parse cmd args
         if len(argv) == 2:
             length = int(argv[0])
             model_base_name = argv[1]
-            num_names = 1
+            num_names = 1  # assume 1 name by default
         elif len(argv) == 3:
             num_names = int(argv[0])
             length = int(argv[1])
@@ -50,8 +63,10 @@ def main(argv):
         else:
             raise ValueError("invalid number of arguments")
 
+        # generate and print desired number of random names
         for i in range(num_names):
             print(generate(length, model_base_name))
+
     except Exception:
         print("Usage: random-name-generator.py [num_names=1] <length> <model_base_name>")
         exit(0)
